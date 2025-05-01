f9bd0286e6438f6cf52748dc1ea0aa84498537ac
Vinodhariharan <vinodhari.ravi@outlook.com>
2025-05-01 20:18:40 +0530

    Update handleImageUpload.js

--- src/component/Admin/handleImageUpload.js
+++ src/component/Admin/handleImageUpload.js
@@ -7,7 +7,7 @@ export const handleImageUpload = async (e, setProduct, productId) => {
 
   try {
     // Step 1: Get a presigned URL from your backend
-    const presignRes = await fetch("http://localhost:5000/api/upload", {
+    const presignRes = await fetch("https://botani-cart-s3-bucket-uploader.vercel.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
